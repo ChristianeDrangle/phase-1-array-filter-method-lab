d36975725e92d228a2f2c1a580ed4a74bef42630
Christiane Drangle <positivitee@proton.me>
2023-04-24 20:30:51 -0400

    Array Filter Lab passing

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+const findMatching = (drivers, string) =>
+  drivers.filter((driver) => driver.toLowerCase() === string.toLowerCase());
+
+const fuzzyMatch = (drivers, string) =>
+  drivers.filter(
+    (driver) => driver.toLowerCase().indexOf(string.toLowerCase()) === 0
+  );
+
+const matchName = (drivers, string) =>
+  drivers.filter((driver) => driver.name === string);
